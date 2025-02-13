2d3536775ed3ec5bb96193957ba40ab816530d66
Przemek Kwiatkowski <kwiatkowskip077@gmail.com>
2025-02-13 21:03:56 +0100

    Added some content to DevicePanel

--- HOME.NECTOR/HOME.NECTOR/src/components/DevicesPanelMain.jsx
+++ HOME.NECTOR/HOME.NECTOR/src/components/DevicesPanelMain.jsx
@@ -1,7 +1,35 @@
+import '../styles/DevicesPanelMainStyle.css'
+import { useState } from 'react';
+
 const DevicesPanelMain = () => {
+
+    const [isDeviceSelected, setIsDeviceSelected] = useState(true);
+
     return (
-        <>
-        </>
+        <div className='devicesPanelMain'>
+            {
+                isDeviceSelected && (
+                    <>
+                        <span className='deviceFullNameHolder'>Multi Sensor Deivce</span>
+                        <div className='deviceParamBoxesHolder'>
+                            <div className='deviceDescriptionAndButtonsHolder'>
+                                <span className='descriptionHolderTitle'>Device Description</span>
+                                <div className='descriptionHolder'>
+                                Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum
+                                </div>
+                                <div className='descriptionButtonsHolder'>
+
+                                </div>
+                            </div>
+                            <div className='deviceDataAndButtonsHolder'>
+
+                            </div>
+                        </div>
+                        <div className='deviceParamBoxesHolder'></div>
+                    </>
+                )
+            }
+        </div>
     )
 }
 
--- HOME.NECTOR/HOME.NECTOR/src/components/DevicesPanelNav.jsx
+++ HOME.NECTOR/HOME.NECTOR/src/components/DevicesPanelNav.jsx
@@ -1,7 +1,18 @@
+import '../styles/DevicesPanelNavStyle.css'
+
 const DevicesPanelNav = () => {
     return (
-        <>
-        </>
+        <div className="devicesPanelNav">
+            <div className='devicesHolder'>
+
+            </div>
+            <button type = "button" className='navPanelBtn'>
+                Connect to Device
+            </button>
+            <button type = "button" className='navPanelBtn'>
+                Disconnect from Device
+            </button>
+        </div>
     )
 }
 
